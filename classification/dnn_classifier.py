57030a7370fea1edfae22d2fcbc8a2d7c3fab813
Multi0825 <doumegurimeguramihe@gmail.com>
2021-08-21 03:59:53 +0900

    Update dnn_classifier.py

--- classification/dnn_classifier.py
+++ classification/dnn_classifier.py
@@ -6,7 +6,7 @@ import torch.nn as nn
 from torch import optim as optimizer
 from torch.utils.data import TensorDataset, DataLoader, Subset
 from sklearn.model_selection import train_test_split
-from classifier_base import _ClassifierBase
+from .classifier_base import _ClassifierBase
 
 # numpy.ndarray->torch.tensor
 # data_type: numpy dtype
