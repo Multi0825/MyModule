c3b97e62924874a931a39902ce9ac57ae263551a
Multi0825 <doumegurimeguramihe@gmail.com>
2022-02-04 14:53:01 +0900

    Update deep4_ae.py

--- dnn/model/deep4_ae.py
+++ dnn/model/deep4_ae.py
@@ -1,7 +1,8 @@
-# Deep4NetをAutoEncoderに改良(修士中間で使用)
-# 層数を1,1.5,2,3,4で変更可能
-# いくつかの引数は削除
+# Deep4NetをAutoEncoderに改良(修士中間で使用した物とほぼ同一,ただしbatch_norm2の位置が前後)
+# 畳み込み層数を1,2,3,4,5で変更可能
+# ＊引数の名前がDeep4Netとも修士で使用したものとも違う
 
+from multiprocessing.sharedctypes import Value
 import torch.nn as nn
 # import modules_d4n_remaked as bd # BraindecodeDeep4Netで使われてたクラス、関数をまとめた
 from .modules_d4n_remaked import Ensure4d, Expression, AvgPool2dWithConv, \
@@ -13,27 +14,26 @@ from .modules_d4n_remaked import Ensure4d, Expression, AvgPool2dWithConv, \
 class Deep4Encoder(nn.Module) :
     '''
     Deep4Encoder
-    ・Deep4Netデフォルトと流れは同じ
-    ・分類器削除
+    ・Deep4Netデフォルトと流れは同じ(分類部分なし)
     ・デフォルトでない分岐を削除、使わない引数削除
     when using encoder only, call single() or switch 'is_single' True
     '''
     def __init__(self, 
-                n_layers,
-                n_filters_time=25, # L1
-                filter_time_length=10,
-                n_filters_spat=25, # L1.5
-                in_chans=14, # 電極数(L1.5)
-                n_filters_2=50, # L2
-                filter_length_2=10,
-                n_filters_3=100, # L3
+                n_convs,
+                n_filters_1=25, # Conv1
+                filter_length_1=10,
+                in_chans=14, # 電極数(filter_length_2)
+                n_filters_2=25, # Conv2
+                n_filters_3=50, # Conv3
                 filter_length_3=10,
-                n_filters_4=200, # L4
+                n_filters_4=100, # Conv4
                 filter_length_4=10,
-                pool_time_length=3, # Pool L1~4 
-                pool_time_stride=3, # Pool L1~4
-                first_nonlin=nn.ELU, # L1,1.5
-                later_nonlin=nn.ELU, # L2~
+                n_filters_5=200, # Conv5
+                filter_length_5=10,
+                pool_time_length=3, # Pool2~ 
+                pool_time_stride=3, # Pool2~
+                first_nonlin=nn.ELU, # 2
+                later_nonlin=nn.ELU, # 3~
                 drop_prob=0.5,
                 batch_norm=True,
                 batch_norm_alpha=0.1,
@@ -42,49 +42,49 @@ class Deep4Encoder(nn.Module) :
                 ) :
         '''
         *Deep4Netに追加した引数のみ記載
-        n_layers: 層数(1, 1.5, 2, 3, 4)
+        n_convs: 畳み込み層数(1, 2, 3, 4, 5)
+        n_filters_N: 各畳み込み層の出力チャンネル数
+        filter_length_N: 各畳み込み層の出力のカーネルサイズに関連(2のみ電極方向)
         size_check: forward時、中間出力のサイズを確認
         is_single: Encoderのみで使用する場合(Decoder用のpool_indices, pool_sizeを返さない)
         '''
         super().__init__() 
         layers = {} 
         conv_stride = 1
-        pool_stride = pool_time_stride
         first_pool_class = nn.MaxPool2d
         later_pool_class = nn.MaxPool2d
-        
-        # L1
+        pool_kernel_size = (pool_time_length,1)
+        pool_stride = (pool_time_stride,1)
+        n_filters = [n_filters_1, n_filters_2, n_filters_3, n_filters_4, n_filters_5]
+        kernel_sizes = [(filter_length_1, 1), (1, in_chans), 
+                        (filter_length_3, 1), (filter_length_4, 1), (filter_length_5, 1)]
+
         layers['dimshuffle'] = Expression(transpose_time_to_spat) # 14 x 1280 x 1 -> 1 x 1280 x 14
-        layers['conv_time'] = nn.Conv2d(in_channels=1, out_channels=n_filters_time, 
-                                        kernel_size=(filter_time_length, 1), stride=1)
-        if n_layers > 1 :
-            # L1.5
-            layers['conv_spat'] = nn.Conv2d(n_filters_time, n_filters_spat, (1,in_chans), 
-                                            stride=(conv_stride,1),bias= not batch_norm) 
-            n_filters_prev = n_filters_spat
-        else :
-            n_filters_prev = n_filters_time
-
+        
+        # Conv1
+        layers['conv_1'] = nn.Conv2d(in_channels=1, out_channels=n_filters[0], 
+                                     kernel_size=kernel_sizes[0], stride=1)
+        # Conv2
+        if n_convs > 1 :
+            layers['conv_2'] = nn.Conv2d(n_filters_1, n_filters[1], kernel_sizes[1], 
+                                            stride=(conv_stride,1), bias= not batch_norm) 
+        n_filters_prev = n_filters[1] if n_convs>1 else n_filters[0]
         if batch_norm:
-            layers['bnorm'] = nn.BatchNorm2d(n_filters_prev, momentum=batch_norm_alpha, affine=True,eps=1e-5) 
-        layers['conv_nonlin'] = first_nonlin() # Activate
-        layers['pool'] = first_pool_class(kernel_size=(pool_time_length,1),stride=(pool_stride,1), return_indices=True) # nn.MaxPool2d
+            layers['bnorm_2'] = nn.BatchNorm2d(n_filters_prev, momentum=batch_norm_alpha, affine=True,eps=1e-5) 
+        layers['nonlin_2'] = first_nonlin() # Activate
+        layers['pool_2'] = first_pool_class(kernel_size=(pool_time_length,1),stride=(pool_stride,1), return_indices=True) # nn.MaxPool2d
 
-        # L2,3,4
-        if n_layers >= 2 :
-            n_filters = [n_filters_2, n_filters_3, n_filters_4]
-            filter_lengthes = [filter_length_2, filter_length_3, filter_length_4]
-            for n_l in range(2, n_layers+1) :
-                layers['drop_{}'.format(n_l)] = nn.Dropout(p=drop_prob) # 通すと出力のどれかが0に
-                layers['conv_{}'.format(n_l)] = nn.Conv2d(n_filters_prev, n_filters[n_l-2], (filter_lengthes[n_l-2], 1),
-                                                          stride=(conv_stride, 1), bias=not batch_norm) 
-                if batch_norm:
-                    layers['bnorm_{}'.format(n_l)] = nn.BatchNorm2d(n_filters[n_l-2],momentum=batch_norm_alpha,
-                                                                    affine=True,eps=1e-5) 
-                layers['nonlin_{}'.format(n_l)] = later_nonlin() # Activate
-                layers['pool_{}'.format(n_l)] = later_pool_class(kernel_size=(pool_time_length, 1),
-                                                                 stride=(pool_stride, 1), return_indices=True) 
-                n_filters_prev = n_filters[n_l-2]
+        # Conv3~5
+        for n_c in range(3, n_convs+1) :
+            layers['drop_{}'.format(n_c)] = nn.Dropout(p=drop_prob) # 通すと出力のどれかが0に
+            layers['conv_{}'.format(n_c)] = nn.Conv2d(n_filters[n_c-2], n_filters[n_c-1], kernel_sizes[n_c-1],
+                                                      stride=(conv_stride, 1), bias=not batch_norm) 
+            if batch_norm:
+                layers['bnorm_{}'.format(n_c)] = nn.BatchNorm2d(n_filters[n_c-1],momentum=batch_norm_alpha,
+                                                                affine=True,eps=1e-5) 
+            layers['nonlin_{}'.format(n_c)] = later_nonlin() # Activate
+            layers['pool_{}'.format(n_c)] = later_pool_class(kernel_size=pool_kernel_size,
+                                                             stride=pool_stride, return_indices=True) 
 
         self.layers = nn.ModuleDict(layers)
         self.size_check = size_check
@@ -92,7 +92,7 @@ class Deep4Encoder(nn.Module) :
     
     # x: Batch x Ch x Value x 1
     def forward(self, x) :
-        # Encoder単体
+        # Encoder単体として
         if self.is_single :
             for name,layer in self.layers.items() :
                 if 'pool' in name :
@@ -102,7 +102,7 @@ class Deep4Encoder(nn.Module) :
                 if self.size_check :
                     print('{}:{}'.format(name, x.size()))
             return x
-        # AEの一部
+        # AEの一部として
         else :
             pool_indices = [] # Unpool用
             pool_size = [] # Unpool用
@@ -119,9 +119,10 @@ class Deep4Encoder(nn.Module) :
     
     def single(self) :
         '''
-        Encoder単体で使用
+        Encoder単体で使用するときに呼び出す
         '''
         self.is_single = True
+
 #########################################################################################################
 # Decoder
 class Deep4Decoder(nn.Module) :
@@ -132,25 +133,25 @@ class Deep4Decoder(nn.Module) :
     ・引数はEncoderと同じ
     '''
     def __init__(self, 
-                n_layers,
-                n_filters_time=25, # L1
-                filter_time_length=10,
-                n_filters_spat=25, # L1.5
-                in_chans=14, # 電極数(L1.5)
-                n_filters_2=50, # L2
-                filter_length_2=10,
-                n_filters_3=100, # L3
+                n_convs,
+                n_filters_1=25, # Deconv1
+                filter_length_1=10,
+                in_chans=14, # 電極数(filter_length_2)
+                n_filters_2=25, # Deconv2
+                n_filters_3=50, # Deconv3
                 filter_length_3=10,
-                n_filters_4=200, # L4
+                n_filters_4=100, # Deconv4
                 filter_length_4=10,
-                pool_time_length=3, # Pool L1~4 
-                pool_time_stride=3, # Pool L1~4
-                first_nonlin=nn.ELU, # L1,1.5
-                later_nonlin=nn.ELU, # L2~
+                n_filters_5=200, # Deconv5
+                filter_length_5=10,
+                pool_time_length=3, # Unpool2~ 
+                pool_time_stride=3, # Unpool2~
+                first_nonlin=nn.ELU, # 2
+                later_nonlin=nn.ELU, # 3~
                 drop_prob=0.5,
                 batch_norm=True,
                 batch_norm_alpha=0.1,
-                size_check=False,               
+                size_check=False,            
                 ) :
         # Decoder: 
         super().__init__()
@@ -159,34 +160,35 @@ class Deep4Decoder(nn.Module) :
         pool_stride = pool_time_stride
         first_pool_class = nn.MaxUnpool2d
         later_pool_class = nn.MaxUnpool2d
+        pool_kernel_size = (pool_time_length,1)
+        pool_stride = (pool_time_stride,1)
+        n_filters = [n_filters_1, n_filters_2, n_filters_3, n_filters_4, n_filters_5]
+        kernel_sizes = [(filter_length_1, 1), (1, in_chans), 
+                        (filter_length_3, 1), (filter_length_4, 1), (filter_length_5, 1)]
         
-        # L4,3,2
-        # *in,outがencoderの逆に
-        # *インデックスが逆順
-        if n_layers >= 2 :
-            n_filters = [n_filters_spat, n_filters_2, n_filters_3, n_filters_4]
-            filter_lengthes = [filter_length_2, filter_length_3, filter_length_4]
-            for n_l in range(n_layers, 1, -1) :
-                layers['unpool_{}'.format(n_l)] = later_pool_class(kernel_size=(pool_time_length, 1),stride=(pool_stride, 1))
-                layers['drop_{}'.format(n_l)] = nn.Dropout(p=drop_prob)
-                layers['deconv_{}'.format(n_l)] = nn.ConvTranspose2d(n_filters[n_l-1], n_filters[n_l-2], (filter_lengthes[n_l-2], 1),
-                                                                        stride=(conv_stride, 1), bias=not batch_norm) 
-                if batch_norm:
-                    layers['bnorm_{}'.format(n_l)] = nn.BatchNorm2d(n_filters[n_l-2], momentum=batch_norm_alpha,
-                                                                    affine=True,eps=1e-5) 
-                layers['nonlin_{}'.format(n_l)] = later_nonlin()
+        # Deconv5~3(*in,outがencoderの逆)
+        for n_c in range(n_convs, 2, -1) : 
+            layers['unpool_{}'.format(n_c)] = later_pool_class(kernel_size=pool_kernel_size,stride=pool_stride)
+            layers['drop_{}'.format(n_c)] = nn.Dropout(p=drop_prob)
+            layers['deconv_{}'.format(n_c)] = nn.ConvTranspose2d(n_filters[n_c-1], n_filters[n_c-2], kernel_sizes[n_c],
+                                                                 stride=(conv_stride, 1), bias=not batch_norm) 
+            if batch_norm:
+                layers['bnorm_{}'.format(n_c)] = nn.BatchNorm2d(n_filters[n_c-2], momentum=batch_norm_alpha,
+                                                                affine=True,eps=1e-5) 
+            layers['nonlin_{}'.format(n_c)] = later_nonlin()
         
-        # L1, 1.5
-        layers['unpool'] = first_pool_class(kernel_size=(pool_time_length,1),stride=(pool_stride,1)) 
-        layers['deconv_nonlin'] = first_nonlin ()
-        n_filters_conv = n_filters_spat if n_layers > 1 else n_filters_time
+        # Deconv2, 1
+        layers['unpool_2'] = first_pool_class(kernel_size=pool_kernel_size,stride=pool_stride) 
+        if n_convs > 1 :
+            layers['deconv_2'] = nn.ConvTranspose2d(n_filters[1], n_filters[0], kernel_sizes[1], 
+                                                    stride=(conv_stride,1),bias= not batch_norm)
+        n_filters_prev = n_filters[1] if n_convs > 1 else n_filters[0] 
         if batch_norm:
-            layers['bnorm'] = nn.BatchNorm2d(n_filters_conv,momentum=batch_norm_alpha,affine=True,eps=1e-5)
-        if n_layers > 1 :
-            layers['deconv_spat'] = nn.ConvTranspose2d(n_filters_spat, n_filters_time, (1,in_chans), 
-                                                       stride=(conv_stride,1),bias= not batch_norm)
-        layers['deconv_time'] = nn.ConvTranspose2d(in_channels=n_filters_time, out_channels=1, 
-                                                   kernel_size=(filter_time_length, 1), stride=1)
+            layers['bnorm_2'] = nn.BatchNorm2d(n_filters_prev, momentum=batch_norm_alpha,affine=True,eps=1e-5)
+        layers['deconv_nonlin_2'] = first_nonlin ()
+        
+        layers['deconv_1'] = nn.ConvTranspose2d(in_channels=n_filters[0], out_channels=1, 
+                                                   kernel_size=kernel_sizes[0], stride=1)
         layers['dimshuffle'] = Expression(transpose_time_to_spat) # 1,3次元を入れ替えるだけなので同じで良し
 
         self.layers = nn.ModuleDict(layers)
@@ -203,6 +205,7 @@ class Deep4Decoder(nn.Module) :
             if self.size_check :
                 print('{}:{}'.format(name, x.size()))
         return x
+
 #########################################################################################################
 
 # Autoencoder
@@ -213,16 +216,16 @@ class Deep4AutoEncoder(nn.Module) :
     ・Deep4Netから分類機能を削除
     ・その他、機能を一部簡略化
     '''
-    def __init__(self, n_layers, **kwargs) :
+    def __init__(self, n_convs, **kwargs) :
         '''
-        n_layers: 層数
+        n_convs: 畳み込み層数
         他引数はEncoder、Decoderで同じ、書くのが面倒なので、**kwargs
         '''
         super().__init__()
-        if n_layers > 4 :
-            raise ValueError('n_layers = 1, 1.5, 2, 3, 4 ')
-        self.encoder = Deep4Encoder(n_layers=n_layers, **kwargs)
-        self.decoder = Deep4Decoder(n_layers=n_layers, **kwargs)
+        if (n_convs<1) or (n_convs>5) :
+            raise ValueError('n_convs = 1, 1.5, 2, 3, 4 ')
+        self.encoder = Deep4Encoder(n_convs=n_convs, **kwargs)
+        self.decoder = Deep4Decoder(n_convs=n_convs, **kwargs)
         self.size_check = kwargs['size_check'] if 'size_check' in list(kwargs.keys()) else False
     
     def forward(self, x) :
@@ -245,6 +248,6 @@ if __name__=='__main__':
     torch.manual_seed(0)
     in_chans = 14
     input = torch.rand((100, in_chans, 1280, 1))
-    model = Deep4AutoEncoder(n_layers=2, size_check=True)
+    model = Deep4AutoEncoder(n_convs=2, size_check=True)
     output = model(input)
     print(output[0,:,0,0])
