dbe04f0a65c6767c02690551406e8f13e1d566fa
Multi0825 <doumegurimeguramihe@gmail.com>
2022-06-06 15:34:28 +0900

    rp

--- features/signal.py
+++ features/signal.py
@@ -27,14 +27,16 @@ def window(data, frame_size, win_type='hamming', overlap_rate=0) :
 def amp_spectrum(data, n=None, min_dim=1, max_dim=None) :
     '''
     FFTにより振幅スペクトル
-    data: np.array(n_sample x n_win x n_ch x n_data)
-    n: データ点(n点では1/(n/sfreq)Hz刻み)
+    data: np.array(n_sample (x n_win) x n_ch x n_data)
+    n: データ長(n点では1/(n/sfreq)Hz刻み)
     min_dim,max_dim: min_dim~max_dim次元まで使用
     '''
     n = data.shape[-1] if n is None else n
     max_dim = n//2 if max_dim is None else max_dim 
     f_fft = fft.fft(data, n=n, axis=data.ndim-1)
-    f_fft = f_fft[:,:,:,min_dim:max_dim+1]
+    if (data.ndim > 4) or (data.ndim < 3) :
+        raise ValueError('data.shape must be (n_sample(,n_win),n_ch,n_data)')
+    f_fft = f_fft[:,:,:,min_dim:max_dim+1] if data.ndim==4 else f_fft[:,:,min_dim:max_dim+1]        
     return np.abs(f_fft) # n_sample x n_win x n_ch x (max_dim+1-min_dim)
 # FFTメモ
 #   データ点の数は2のべき乗であるべき(らしい)
@@ -57,8 +59,38 @@ def dwt(data, wavelet='db4', level=4, mode='zero', cA=False) :
     cDs = [np.array(coefs[i]) for i in range(cA,len(coefs))] # 粗⇔細(cA, cD1,...cDn)
     return cDs # n_dec * n_ch * n_feat(0次元はリスト) 
 
+def relative_phases(data, n=None, std_dim=5) :
+    '''
+    相対位相(坂井)
+    基準周波数の位相で正規化、他の位相を相対的に求める？
+    data: 信号データ(n_sample x n_win x n_ch x n_data)
+    n: データ長
+    std_dim: 基準次元(default: 5)
+    '''
+    n = data.shape[-1] if n is None else n # データ長
+    ddim = data.ndim
+    fft_f = fft.fft(data, n=n, axis=data.ndim-1) # FFT(最終次元にそって)
+    if (ddim > 4) or (ddim < 3) :
+        raise ValueError('data.shape must be (n_sample(,n_win),n_ch,n_data)')
+    fft_f = fft_f[:,:,:,1:n//2+1] if ddim==4 else fft_f[:,:,:,1:n//2+1]# 対称部分を切り捨て(0も直流成分のため切り捨て)
+    phases = np.angle(fft_f) # 位相に変換
+    standard_phases = phases[:,:,:,std_dim] if ddim==4 else phases[:,:,std_dim]# 各chの基準周波数の位相
+    relative_phases = [] # 相対位相
+    for dim in range(phases.shape[-1]) :
+        phase = phases[:,:,:,dim] if ddim==4 else phases[:,:,dim]
+        r_p = phase - (dim/std_dim * standard_phases) 
+        relative_phases = np.expand_dims(r_p, ddim-1) if dim==0 \
+                          else np.concatenate((relative_phases, np.expand_dims(r_p,ddim-1)), axis=ddim-1)
+    # 位相値を対応する座標値に変換
+    sins = np.sin(relative_phases)
+    coses = np.cos(relative_phases)
+    return sins, coses # n_data x n_ch x ori_data_len/2
+
 if __name__=='__main__' :
     data = np.random.rand(100, 14, 256)
-    new_data = window(data, frame_size=128, overlap_rate=0.5) # n_batch x 3 x n_ch x frame_size
-    new_data = amp_spectrum(new_data, n=128)
+    win_data = window(data, frame_size=128, overlap_rate=0.5) # n_batch x 3 x n_ch x frame_size
+    new_data = amp_spectrum(win_data, n=128)
     print(new_data.shape)
+    sin,cos = relative_phases(win_data,std_dim=5)
+    print(sin.shape)
+    print(cos.shape)
