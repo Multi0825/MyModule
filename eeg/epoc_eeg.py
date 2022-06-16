b294a1d9974ab7a613db5d9512962853e866cb3c
Multi0825 <doumegurimeguramihe@gmail.com>
2022-06-16 17:43:14 +0900

    Update epoc_eeg.py

--- eeg/epoc_eeg.py
+++ eeg/epoc_eeg.py
@@ -298,15 +298,13 @@ class EpocEEG():
                 for e in range(self.n_epoch) :
                     for n_stg in range(len(self.stages)) :
                         # resting,0 -> ... -> speaking,0 -> resting, 1 -> ...
-
                         next_stg = self.stages[0] if n_stg==len(self.stages)-1 else self.stages[n_stg+1] 
                         start = self.stage_starts[self.stages[n_stg]][e]
                         if e == self.n_epoch-1 :
                             next_stg = n_stg if n_stg==len(self.stages)-1 else self.stages[n_stg+1]
-                            end = self.stage_starts[next_stg][e]-1
+                            end = len(label)-1 if n_stg==len(self.stages)-1 else self.stage_starts[next_stg][e]-1
                         else :
                             end = self.stage_starts[next_stg][e+1]-1 if n_stg==len(self.stages)-1 else self.stage_starts[next_stg][e]-1
-                        
                         stage.extend([self.stages[n_stg] for i in range(end-start+1)])
             else :
                 stage = [self.stages[0] for t in range(len(time))]
