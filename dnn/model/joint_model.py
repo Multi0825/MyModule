a302df9e036eac59f842c135f90a187b1c5318d9
Multi0825 <doumegurimeguramihe@gmail.com>
2022-07-11 13:52:32 +0900

    Update joint_model.py

--- dnn/model/joint_model.py
+++ dnn/model/joint_model.py
@@ -4,15 +4,17 @@ import torch.nn as nn
 
 class JointModel(nn.Module) :
     '''
-    サブモデルの出力をメインへの入力に結合
+    2つのモデルを結合
     '''
-    def __init__(self, main, sub, joint_dim=-1, joint_func=None, train_mode='double') :
+    def __init__(self, main, sub, joint_dim=-1, joint_func=None, train_mode='double', stream='parallel') :
         '''
         main: 最終出力を行うモデル(インスタンス)
         sub: 出力がメインへの入力に結合されるモデル(インスタンス)
-        joint_dim: サブ出力と入力の結合次元(-1でAuto, joint_layerを指定した場合使用しない)
+        joint_dim: サブ出力と入力の結合次元(-1でAuto, joint_funcを指定した場合使用しない)
         joint_func: サブの出力と入力の結合を行う関数(Noneの場合, torch.cat((x,subout),joint_dim))
         train_mode: 'double':両方, 'main':メインのみ, 'sub':サブのみ 
+        stream: 'parallel'= x->[sub]->subout x+subout->[main]->mainout
+                'series'=   x=>[sub]->subout   subout->[main]->mainout
         '''
         super().__init__()
         self.main = copy.deepcopy(main)
@@ -20,18 +22,24 @@ class JointModel(nn.Module) :
         self.joint_dim = joint_dim 
         self.joint_func = joint_func
         self.train_mode = train_mode
+        self.stream = stream
         
     def forward(self, x) :
         # サブ
         subout = self.sub(x)
-        # 入力結合
-        if self.joint_func is not None :
-            x = self.joint_func(x, subout)
+        if self.stream=='paralell':
+            # 入力結合
+            if self.joint_func is not None :
+                x = self.joint_func(x, subout)
+            else :
+                dim = x.dim()-1 if self.joint_dim==-1 else self.joint_dim
+                x = torch.cat((x,subout), dim)
+            # メイン
+            mainout = self.main(x)
+        elif self.stream=='series' :
+            mainout = self.main(subout)
         else :
-            dim = x.dim()-1 if self.joint_dim==-1 else self.joint_dim
-            x = torch.cat((x,subout), dim)
-        # メイン
-        mainout = self.main(x)
+            raise ValueError('stream is "parallel" or "series"')
         return mainout
 
     def train(self, mode=True) :
